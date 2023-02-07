ee9736b5f4bda5e29d2430776f263d2ce7e103d7
Tika Dzamikh <dzamikh@protonmail.com>
2023-02-07 20:41:58 +0300

    Work with logic

--- RGBView/ViewController.swift
+++ RGBView/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet var customizedView: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        customizedView.layer.cornerRadius = 16
+        
+        sliderValues()
+        labelValues()
     }
 
-
+    //MARK: - IBActions
+    @IBAction func redSliderAction() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        changeColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        changeColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        changeColor()
+    }
+    
+    // MARK: - Private Methods
+    private func sliderValues () {
+        redSlider.value = 0.2
+        greenSlider.value = 0.4
+        blueSlider.value = 0.6
+    }
+    
+    private func labelValues () {
+        redLabel.text = redSlider.value.formatted()
+        greenLabel.text = greenSlider.value.formatted()
+        blueLabel.text = blueSlider.value.formatted()
+    }
+    
+    private func changeColor () {
+        customizedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 }
 
